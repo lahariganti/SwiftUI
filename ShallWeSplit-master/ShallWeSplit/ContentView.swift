e4b393eb7b1c9afbbe83aad06aa521646a407ff6
Lahari Ganti <gplahari@gmail.com>
2021-03-22 10:19:06 +0100

    k

---
+++ ShallWeSplit-master/ShallWeSplit/ContentView.swift
@@ -0,0 +1,63 @@
+//
+//  ContentView.swift
+//  ShallWeSplit
+//
+//  Created by Lahari Ganti on 10/23/19.
+//  Copyright © 2019 Lahari Ganti. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	@State private var checkAmount = ""
+	@State private var tipPercentage = 2
+	@State private var numberOfPeople = 2
+
+	let tipPercentages = [5, 10, 15, 20, 0]
+
+	var totalPerPerson: Double {
+		let peopleCount = Double(numberOfPeople + 2)
+		let tipSelection = Double(tipPercentages[tipPercentage])
+		let orderAmount = Double(checkAmount) ?? 0
+		let tipValue = orderAmount / 100 * tipSelection
+		let grandTotal = orderAmount + tipValue
+		let amountPerPerson = grandTotal / peopleCount
+		return amountPerPerson
+	}
+
+    var body: some View {
+		NavigationView {
+			Form {
+				Section {
+					TextField("Check Amount", text: $checkAmount)
+						.keyboardType(.decimalPad)
+				}
+
+				Picker("Select number of people", selection: $numberOfPeople) {
+					ForEach(2 ..< 100) {
+						Text("\($0 ) people")
+					}
+				}
+
+				Section(header: Text("How much tip do you want to leave?")) {
+					Picker("Tip Percentage", selection: $tipPercentage) {
+						ForEach(0 ..< tipPercentages.count) {
+							Text("\(self.tipPercentages[$0]) %")
+						}
+					}.pickerStyle(SegmentedPickerStyle())
+				}
+
+
+				Section {
+					Text("$ \(totalPerPerson, specifier: "%.2f")")
+				}
+			}.navigationBarTitle("ShallWeSplit")
+		}
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
